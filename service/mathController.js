c9326090005553e4a93c0bb1f30e0f21ef05700f
oraclepxx <oraclepxx@gmail.com>
2015-04-06 13:07:55 -0700

    -update

--- service/mathController.js
+++ service/mathController.js
@@ -3,20 +3,48 @@
  */
 
 mainApp.controller("mathController", function ($scope, MathService) {
+
+    checkInput = function () {
+        if ($scope.inputNum1 == null || $scope.inputNum2 == null) {
+            return false;
+        }
+        return true;
+    }
+
+    showAlert = function () {
+        alert("Input cannot be empty.")
+    }
+
     $scope.add = function () {
-        $scope.result = MathService.add($scope.inputNum1, $scope.inputNum2);
+        if (checkInput()) {
+            $scope.result = MathService.add($scope.inputNum1, $scope.inputNum2);
+        } else {
+            showAlert();
+        }
     }
 
     $scope.subtract = function () {
-        $scope.result = MathService.subtract($scope.inputNum1, $scope.inputNum2);
+        if (checkInput()) {
+            $scope.result = MathService.subtract($scope.inputNum1, $scope.inputNum2);
+        } else {
+            showAlert();
+        }
     }
 
     $scope.multi = function () {
-        $scope.result = MathService.multi($scope.inputNum1, $scope.inputNum2);
+        if (checkInput()) {
+            $scope.result = MathService.multi($scope.inputNum1, $scope.inputNum2);
+        } else {
+            showAlert();
+        }
     }
 
     $scope.divide = function () {
-        $scope.result = MathService.divide($scope.inputNum1, $scope.inputNum2);
+        if (checkInput()) {
+            $scope.result = MathService.divide($scope.inputNum1, $scope.inputNum2);
+        } else {
+            showAlert();
+        }
     }
 
 });
