89fd2d26f0875097536e5e0bf1b89a4cfc9b14d3
oraclepxx <oraclepxx@gmail.com>
2015-04-07 21:02:59 -0700

    -update

--- directive/mainApp.js
+++ directive/mainApp.js
@@ -4,16 +4,55 @@
 
 var mainApp = angular.module("mainApp", []);
 
-mainApp.directive("my-directive", function () {
+mainApp.directive("mydirective", function () {
 
     return {
-        restrict: "A",
-        templateUrl: "<h1>This is my first custom directive</h1>"
+        restrict: "E",
+        template: "<h1>This is a custom directive</h1>"
     };
 
-    //var directive = {};
-    //directive.restrict = "A";
-    //directive.template = "<h1>This is my first custom directive.</h1>";
-    //return directive;
+    //var mydirective = {};
+    //mydirective.restrict = "A";
+    //mydirective.template = "<h1>This is my first custom directive.</h1>";
+    //return mydirective;
 
+});
+
+mainApp.directive('hello1', function () {
+    return {
+        restrict: 'A',
+        replace: false,
+        template: '<span>NotReplace</span>'
+    };
+});
+
+mainApp.directive('hello2', function () {
+    return {
+        restrict: 'A',
+        replace: true,
+        template: '<span>Replace</span>'
+    };
+});
+
+mainApp.directive('hello3', function () {
+    return {
+        restrict: 'A',
+        template: '<span>Replace?NotReplace</span>'
+    };
+});
+
+mainApp.directive('myd1', function () {
+    return {
+        restrict: 'A',
+        template: '<span>directive template1</span>',
+        replace: true
+    }
+});
+
+mainApp.directive('myd2', function () {
+    return {
+        restrict: 'A',
+        template: '<span>directive template2</span>',
+        replace: false
+    }
 });
